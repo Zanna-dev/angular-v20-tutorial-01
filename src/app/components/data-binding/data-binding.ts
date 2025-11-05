98c638c85a36f811969f6ce5ac17f5119152ae34
Zanna-dev <pamn2704@gmail.com>
2025-11-05 21:46:09 +0100

    Implement Data Binding in Angular 20

---
+++ src/app/components/data-binding/data-binding.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-data-binding',
+  imports: [FormsModule],
+  templateUrl: './data-binding.html',
+  styleUrl: './data-binding.css',
+})
+export class DataBinding {
+  courseName : string = "Angular 20 Tutorial";
+  productPrice : number = 12500;
+  maxLength : number = 5;
+  minChar: number = 3;
+  inputType: string = "checkbox";
+  myClassName : string = "myColor"
+
+  constructor() {
+    
+  }
+
+  showWelcomeMessage() {
+    alert("Welcome to Angular 20")
+  }
+
+  changeCourseName() {
+    this.courseName = "Reacrjs Tutorials"
+  }
+
+  onCityChange() {
+    alert("City Change")
+  }
+
+}
