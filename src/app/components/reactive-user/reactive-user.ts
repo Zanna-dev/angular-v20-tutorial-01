ec960a5678346cd74e7f1c721b65c7bdb3c19a82
Zanna-dev <pamn2704@gmail.com>
2025-11-10 22:37:15 +0100

    Implement Reactive Form in Angular 20

---
+++ src/app/components/reactive-user/reactive-user.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-user',
+  imports: [ReactiveFormsModule],
+  templateUrl: './reactive-user.html',
+  styleUrl: './reactive-user.css',
+})
+export class ReactiveUser implements OnInit {
+
+  userList: any[] = [];
+  http = inject(HttpClient);
+
+  userForm: FormGroup = new FormGroup({
+   userId: new FormControl(0),
+   emailId: new FormControl(""),
+   password: new FormControl(""),
+   fullName: new FormControl(""),
+   mobileNo: new FormControl(""),
+  });
+
+  ngOnInit(): void {
+    this.getUsers()
+  }
+
+  getUsers() {
+    this.http.get("https://api.freeprojectapi.com/api/GoalTracker/getAllUsers").subscribe((res: any) => {
+      this.userList = res;
+    });
+  }
+
+ onSaveUser() {
+    debugger;
+    const formValue = this.userForm.value
+    this.http.post("https://api.freeprojectapi.com/api/GoalTracker/register", formValue).subscribe({
+      next: () => {
+        alert("User Registered Successfully");
+        this.getUsers();
+      },
+      error: (error) => {
+        alert("Error Occured" + error);
+      }
+    }
+    )
+  }
+
+}
